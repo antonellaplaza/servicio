964d8e77327b8738c457856896154a7b01256416
antonella <antonellaplaza2022@gmail.com>
2025-06-09 21:44:08 -0400

    correciones

--- js/script.js
+++ js/script.js
@@ -1,16 +1,1 @@
 
-// Botón para volver arriba
-const btnArriba = document.getElementById("btn-arriba");
-
-window.onscroll = () => {
-  if (document.documentElement.scrollTop > 100) {
-    btnArriba.style.display = "block";
-  } else {
-    btnArriba.style.display = "none";
-  }
-};
-
-btnArriba.addEventListener("click", () => {
-  window.scrollTo({ top: 0, behavior: "smooth" });
-});
-
