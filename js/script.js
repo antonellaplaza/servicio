82cab33ae19ddc5c1322db642b0b4dbf09fae0fa
antonella <antonellaplaza2022@gmail.com>
2025-06-25 21:52:03 -0400

    cambio de tema

--- js/script.js
+++ js/script.js
@@ -1,1 +1,11 @@
+function ampliarImagen(img) {
+    const visor = document.getElementById("visor");
+    const imagenAmpliada = document.getElementById("imagen-ampliada");
+    imagenAmpliada.src = img.src;
+    visor.style.display = "flex";
+}
+
+function cerrarImagen() {
+    document.getElementById("visor").style.display = "none";
+}
 
